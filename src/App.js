c6421de93431e152a1b8cf589d91910011dcf3e7
Borislav Todorov <btodorov@axway.com>
2020-02-26 15:31:42 +0200

    Repos page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -11,6 +12,8 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -19,6 +22,22 @@ class App extends Component {
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
     setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
   };
 
   searchUsers = async text => {
@@ -30,32 +49,45 @@ class App extends Component {
   };
 
   render() {
+    const { users, repos, loading, user, alert } = this.state;
+
     return (
       <Router>
-        <div className="App">
+        <div className='App'>
           <Navbar />
-          <div className="container">
-            <Alert alert={this.state.alert} />
+          <div className='container'>
+            <Alert alert={alert} />
             <Switch>
               <Route
                 exact
-                path="/"
+                path='/'
                 render={props => (
                   <Fragment>
                     <Search
                       searchUsers={this.searchUsers}
                       clearUsers={this.clearUsers}
-                      showClear={this.state.users.length > 0}
+                      showClear={users.length > 0}
                       setAlert={this.setAlert}
                     />
-                    <Users
-                      loading={this.state.loading}
-                      users={this.state.users}
-                    />
+                    <Users loading={loading} users={users} />
                   </Fragment>
                 )}
               ></Route>
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = ({ user: { avatar_url, login, html_url } }) => {
   return (
@@ -13,16 +14,16 @@ const UserItem = ({ user: { avatar_url, login, html_url } }) => {
       <h3>{login}</h3>
 
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
 };
 
 UserItem.propTypes = {
-  user: PropTypes.object.isRequired,
-}
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
